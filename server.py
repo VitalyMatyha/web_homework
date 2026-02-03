c50b2110fd8939c24ed9e6ced3b9ce95e9fd1735
Vitaly_M <vitalymatyha@gmail.com>
2026-02-03 23:59:43 +0300

    ad all

---
+++ server.py
@@ -0,0 +1,18 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html; charset=utf-8")
+        self.end_headers()
+
+        with open("contacts.html", "r", encoding="utf-8") as file:
+            html = file.read()
+
+        self.wfile.write(html.encode("utf-8"))
+
+
+server = HTTPServer(("localhost", 8000), Handler)
+print("http://localhost:8000")
+server.serve_forever()
