99bd2031c1ddba997d9b4ddb4de7fef4a61a82bf
Vitaly_M <vitalymatyha@gmail.com>
2026-02-25 19:19:37 +0300

    add change

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('', include('catalog.urls')),  # все маршруты из catalog
 ]
 
-
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
